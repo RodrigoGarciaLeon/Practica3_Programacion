5ecbe5583aa02f50597bb15dadae3853bb31bcc1
RodrigoGarciaLeon <rodrigarl46@gmail.com>
2022-03-06 20:23:54 +0000

    Ejercicio 5

--- src/agregadas/MiAsignatura.java
+++ src/agregadas/MiAsignatura.java
@@ -1,49 +1,45 @@
-package agregadas;	
+package agregadas;
 
-public class MiAsignatura {
-	private String nombre;
-	private Estudiante[] estudiantes;
-	private int numeroEstudiantes;
+	public class MiAsignatura {
+		private String nombre;
+		private Estudiante[] estudiantes;
+		private int numeroEstudiantes;
 
-	public MiAsignatura(String nombre, int maximoNumeroDeEstudiantes) {
-		super();
-		this.nombre = nombre;
-		this.estudiantes = new Estudiante[maximoNumeroDeEstudiantes];
-		this.numeroEstudiantes = 0;
-	}
-
-	
-	/** 
-	 * @param estudiante
-	 * @return int
-	 */
-	// TODO: A�adir documentaci�n para javadoc
-    public int anadirEstudiante(Estudiante estudiante){	
-		if(numeroEstudiantes == estudiantes.length){
-			return -1;
+		public MiAsignatura(String nombre, int maximoNumeroDeEstudiantes) {
+			super();
+			this.nombre = nombre;
+			this.estudiantes = new Estudiante[maximoNumeroDeEstudiantes];
+			this.numeroEstudiantes = 0;
 		}
-		for(int i=0; i<estudiantes.length; i++){
-			estudiantes[i] = estudiante;
-			numeroEstudiantes++;
-		}	
-		return 1;
+
+		
+		/** 
+		 * @param estudiante
+		 * @return int
+		 */
+		// TODO: A�adir documentaci�n para javadoc
+		public int anadirEstudiante(Estudiante estudiante){	
+			if(numeroEstudiantes == estudiantes.length){
+				return -1;
+			}
+			for(int i=0; i<estudiantes.length; i++){
+				estudiantes[i] = estudiante;
+				numeroEstudiantes++;
+			}return 1;
+		}
+
+		
+		/** 
+		 * @param desdeEdad
+		 * @param hastaEdad
+		 */
+		// TODO: A�adir documentaci�n para javadoc
+		public void mostrarEstudiantesConEdad (int desdeEdad, int hastaEdad) {
+			for (int i=0; i<estudiantes.length; i++) {
+				if (estudiantes[i].getEdad() <= hastaEdad && estudiantes[i].getEdad() >= desdeEdad ) {
+					System.out.println(estudiantes[i].getEdad() + " " + estudiantes[i].getNombre());
+			}
+			System.out.println();
+		}
 	}
-
-	
-	/** 
-	 * @param desdeEdad
-	 * @param hastaEdad
-	 */
-	// TODO: A�adir documentaci�n para javadoc
-
-
-	public void mostrarEstudiantesConEdad (int desdeEdad, int hastaEdad) {
-		for (int i=0; i<estudiantes.length; i++) {
- 			if (estudiantes[i].equals(desdeEdad) && estudiantes[i].equals(hastaEdad) ) {
- 				System.out.print("edad " + estudiantes[i] );
- 		}
- 		System.out.println();
-	 }
-
- }
 }
